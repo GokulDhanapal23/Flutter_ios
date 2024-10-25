4b9f3992ee2f287d457f1832786f9aafac760152
Gokul Dhanapal <gokuldhanapal23@gmail.com>
2024-10-25 20:21:51 +0530

    Shop Order Changes

--- Bisca360/lib/Screen/Settings.dart
+++ Bisca360/lib/Screen/Settings.dart
@@ -18,7 +18,6 @@ class _SettingsScreenState extends State<Settings> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        centerTitle: true,
         backgroundColor: Colors.green,
         leading: IconButton(
           onPressed: () => Navigator.of(context).pop(),
@@ -29,32 +28,32 @@ class _SettingsScreenState extends State<Settings> {
       body: ListView(
         padding: EdgeInsets.all(16.0),
         children: [
-          ListTile(
-            title: Text('Enable notifications'),
-            subtitle: Text('Receive alerts for updates'),
-            leading: Icon(Icons.notifications),
-            trailing: Switch(
-              value: _notificationsEnabled,
-              onChanged: (bool value) {
-                setState(() {
-                  _notificationsEnabled = value;
-                });
-              },
-            ),
-          ),
-          ListTile(
-            title: Text('Dark Mode'),
-            subtitle: Text('Switch between light and dark themes'),
-            leading: Icon(Icons.brightness_2),
-            trailing: Switch(
-              value: _darkModeEnabled,
-              onChanged: (bool value) {
-                setState(() {
-                  _darkModeEnabled = value;
-                });
-              },
-            ),
-          ),
+          // ListTile(
+          //   title: Text('Enable notifications'),
+          //   subtitle: Text('Receive alerts for updates'),
+          //   leading: Icon(Icons.notifications),
+          //   trailing: Switch(
+          //     value: _notificationsEnabled,
+          //     onChanged: (bool value) {
+          //       setState(() {
+          //         _notificationsEnabled = value;
+          //       });
+          //     },
+          //   ),
+          // ),
+          // ListTile(
+          //   title: Text('Dark Mode'),
+          //   subtitle: Text('Switch between light and dark themes'),
+          //   leading: Icon(Icons.brightness_2),
+          //   trailing: Switch(
+          //     value: _darkModeEnabled,
+          //     onChanged: (bool value) {
+          //       setState(() {
+          //         _darkModeEnabled = value;
+          //       });
+          //     },
+          //   ),
+          // ),
           ListTile(
             title: Text('Setup MPIN'),
             leading: Icon(Icons.password),
@@ -66,22 +65,43 @@ class _SettingsScreenState extends State<Settings> {
               ),
               Divider(),
               ListTile(
-              title: Text('Account'),
-              subtitle: Text('Manage your account settings'),
+              title: Text('App version Code'),
+                leading: Icon(Icons.code),
+              subtitle: Text('2024102520'),
               onTap: () {
               // Navigate to account settings
               },
               ),
-              ListTile(
-              title: Text('Privacy'),
-              subtitle: Text('Control your privacy settings'),
+          Divider(),
+          ListTile(
+              title: Text('App Version'),
+            leading: Icon(Icons.app_settings_alt),
+              subtitle: Text('1.0.0+1'),
               onTap: () {
-              // Navigate to privacy settings
-
-            },
-          ),
+              // Navigate to account settings
+              },
+              ),
+          // Divider(),
+          // ListTile(
+          //     title: Text('Account'),
+          //     subtitle: Text('Manage your account settings'),
+          //     onTap: () {
+          //     // Navigate to account settings
+          //     },
+          //     ),
+          // Divider(),
+          //     ListTile(
+          //     title: Text('Privacy'),
+          //     subtitle: Text('Control your privacy settings'),
+          //     onTap: () {
+          //     // Navigate to privacy settings
+          //
+          //   },
+          // ),
+          Divider(),
           ListTile(
             title: Text('About'),
+            leading: Icon(Icons.info_outline),
             subtitle: Text('Learn more about the app'),
             onTap: () {
               // Navigate to about page
